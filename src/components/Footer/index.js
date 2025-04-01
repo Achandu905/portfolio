1aa8fdf81a95d031699081808793db54bc9392d4
unknown <ajmeerachandu2001@gmail.com>
2025-04-01 21:41:07 +0530

    Updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -103,7 +103,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2024 Chandu Ajmeera. All rights reserved.
+          &copy; 2025 Chandu Ajmeera. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
